d59c11958b4bbd57630aa60ef29200ccb555ba54
Raivo Lapiņš <lapinna.inese@gmail.com>
2022-07-30 23:36:09 +0300

    Pushing code to VCS

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeColor() {
+    document.getElementById("top").style.color = "purple";
+
+} 
+
+function changeSize() {
+    document.getElementById("inside").style.fontSize= "20px";
+
+}
+
+function changeStyle() {
+    document.getElementById("bottom").style.fontStyle= "italic";
+
+}
